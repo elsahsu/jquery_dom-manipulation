7c12a718b0b2e7058b1b154fddf90eafe866d0b6
Elsa Hsu <elsanfhsu@gmail.com>
2018-02-06 23:14:01 +0200

    modifying css

---
+++ modifying_css/modifying_css.js
@@ -0,0 +1,9 @@
+/*
+Change the font-size of all the article-items to 20px!
+
+Must use jQuery's css() method!
+*/
+
+// Start with this variable!
+var articleItems;
+articleItems = $('.article-item').css("font-size", "20px");
