3e6f07f0b2271246f8ae39c05ec9d71ea288fdf4
Elsa Hsu <elsanfhsu@gmail.com>
2018-02-06 23:21:12 +0200

    removing dom elements

---
+++ removing_dom_elements/removing_dom_elements.js
@@ -0,0 +1,14 @@
+/*
+Remove the <ul> from the first article item!
+
+Must use jQuery's remove() method.
+*/
+
+// Start with this variable!
+var articleItems;
+
+articleItems = $('h3').siblings('ul').remove();
+
+//$( ".hello" ).remove();
+//$( "div" ).remove( ".hello" );
+//Sample Image Title here
